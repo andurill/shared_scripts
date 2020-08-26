c3db465f43844ca855445e1216a9700f20fc30e8
andurill <gowtham.jn@gmail.com>
2020-08-26 14:41:55 -0400

    allow single project/run

--- fpcor.r
+++ fpcor.r
@@ -4,6 +4,7 @@ suppressPackageStartupMessages({
   require(argparse)
   require(viridisLite)})
 
+
 reformat_fp = function(tf, fp, include_patientID, controls, dtype) {
   if (controls == "" |
       (!all(is.element(unique(strsplit(controls, ",")[[1]]),
@@ -34,7 +35,7 @@ reformat_fp = function(tf, fp, include_patientID, controls, dtype) {
   fp = fp[,c("Locus", grep(non_controls, names(fp), value = T))]
 
   if (!is.null(include_patientID)) {
-    check_valid_sample(include_patientID, tf, "source")
+    check_valid_sample(include_patientID, tf, dtype)
     filter_pattern = paste0(paste(strsplit(include_patientID, ",")[[1]], 
 			   collapse = ".*MinorAlleleFreq|"), ".*MinorAlleleFreq")
   } else  {
@@ -46,6 +47,7 @@ reformat_fp = function(tf, fp, include_patientID, controls, dtype) {
   return(fp)
 }
 
+
 check_valid_sample = function(samples, tf, dtype) {
   for (s in strsplit(samples, ",")[[1]]) {
     if (!s %in% tf$Patient_ID) {
@@ -54,29 +56,47 @@ check_valid_sample = function(samples, tf, dtype) {
   }
 }
 
+
+process_data = function(fp, tf, pID, exclude_controls, dtype) {
+  files = lapply(c(tf, fp), function(i){
+    read.csv(i, sep="\t", header = T, colClasses = "character")})
+  processed_fp = reformat_fp(files[[1]], files[[2]], pID, exclude_controls, dtype)
+  return(processed_fp)
+}
+
+
 corr_fp = function(source_fp, source_tf, reference_fp, 
 		   reference_tf, source_patientID, reference_patientID, 
 		   exclude_controls, outfile) {
-  files = lapply(c(source_tf, reference_tf, source_fp, reference_fp), function(i){
-    read.csv(i, sep="\t", header = T, colClasses = "character")})
   
-  source_fp = reformat_fp(files[[1]], files[[3]], 
-			  source_patientID, exclude_controls, "source")
+  source_fp = process_data(source_fp, source_tf, source_patientID,
+			   exclude_controls, "source")
+  
+  if (!(is.null(reference_fp) | is.null(source_fp))) {
+    reference_fp = process_data(reference_fp, reference_tf, reference_patientID,
+				exclude_controls, "reference")
 
-  reference_fp = reformat_fp(files[[2]], files[[4]],
-			     reference_patientID, exclude_controls, "reference")
+    common_locus = intersect(source_fp$Locus, reference_fp$Locus)
 
-  common_locus = intersect(source_fp$Locus, reference_fp$Locus)
-  fp_matrix = as.data.frame(
-    cbind(
-      sapply(source_fp[which(source_fp$Locus %in% common_locus),
-                       c(grep("Locus", names(source_fp), 
-                              value = T, invert = T))], as.numeric, na.rm=T),
-      sapply(reference_fp[which(reference_fp$Locus %in% common_locus),
-                          c(grep("Locus", names(reference_fp), 
-                                 value = T, invert = T))], as.numeric, na.rm=T)
+    fp_matrix = as.data.frame(
+      cbind(
+        sapply(source_fp[which(source_fp$Locus %in% common_locus),
+                         c(grep("Locus", names(source_fp), 
+                                value = T, invert = T))], as.numeric, na.rm=T),
+        sapply(reference_fp[which(reference_fp$Locus %in% common_locus),
+                            c(grep("Locus", names(reference_fp), 
+                                   value = T, invert = T))], as.numeric, na.rm=T)
       )
     )
+  }
+  else {
+    common_locus = source_fp$Locus
+    fp_matrix = as.data.frame(
+      sapply(source_fp[which(source_fp$Locus %in% common_locus),
+	     c(grep("Locus", names(source_fp), value = T, invert = T))],
+      as.numeric, na.rm=T))
+  }
+
   names(fp_matrix) = gsub("\\.", "-", 
                           gsub("^X","", 
                                gsub("_MinorAlleleFreq", "", names(fp_matrix))))
@@ -101,10 +121,10 @@ parser$add_argument('-st', '--source_title_file', required = TRUE,
                     help = 'Title file of the source project')
 parser$add_argument('-sp', '--source_FP_file', required = TRUE,
                     help = 'Fingerprint SNP file of the source project')
-parser$add_argument('-rt', '--reference_title_file', required = TRUE,
-                    help = 'Title file of the reference project')
-parser$add_argument('-rp', '--reference_FP_file', required = TRUE,
-                    help = 'Fingerprint SNP file of the reference project')
+parser$add_argument('-rt', '--reference_title_file', default = NULL, 
+		    help = 'Title file of the reference project')
+parser$add_argument('-rp', '--reference_FP_file', default = NULL, 
+		    help = 'Fingerprint SNP file of the reference project')
 parser$add_argument('-ss', '--source_samples', default = NULL,
                     help = 'comma-separated list of MRNs/PatientIDs that should be selected from the source project for analysis')
 parser$add_argument('-rs', '--reference_samples', default = NULL,
